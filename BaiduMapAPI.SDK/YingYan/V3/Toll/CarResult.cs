39213267e83e9e934daf1cce3b353ee9d9de1331
狮主 <405415435@qq.com>
2022-04-08 17:56:35 +0800

    增加【ETC费用计算服务】接口对接

---
+++ BaiduMapAPI.SDK/YingYan/V3/Toll/CarResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaiduMapAPI.YingYan.V3.Toll
+{
+    /// <summary>
+    /// 计算乘用车ETC费用 结果
+    /// </summary>
+    public class CarResult : Models.ResponseOld
+    {
+        /// <summary>
+        /// 总ETC费用
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("toll")]
+        public double? Toll { get; set; }
+
+        /// <summary>
+        /// 收费区间数量
+        /// <para>收费区间包括：<br/>
+        /// 1. 一段以收费站入口开始和收费站出口结束的高速区间<br/>
+        /// 2. 只在入口收费或只在出口收费的高速区间<br/>
+        /// 3. 收费的桥梁
+        /// </para>
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("toll_section_num")]
+        public int? TollSectionNumber { get; set; }
+
+
+        /// <summary>
+        /// 分段收费信息
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("toll_sections")]
+        public List<TollSection> TollSections { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -54,3 +54,4 @@ var result = search.GetResult(AK, SK);
 23 | BaiduMapAPI.YingYan.V3.Track | [鹰眼-轨迹上传](http://lbsyun.baidu.com/index.php?title=yingyan/api/v3/trackupload)、[鹰眼-轨迹查询和纠偏](http://lbsyun.baidu.com/index.php?title=yingyan/api/v3/trackprocess)
 24 | BaiduMapAPI.YingYan.V3.Analysis | [鹰眼-轨迹分析](https://lbsyun.baidu.com/index.php?title=yingyan/api/v3/analysis)
 25 | BaiduMapAPI.YingYan.V3.FrequentRoute | [鹰眼-经验行为分析](https://lbsyun.baidu.com/index.php?title=yingyan/api/v3/empiricalbehavior)
+26 | BaiduMapAPI.YingYan.V3.Toll | [鹰眼-ETC费用计算服务](https://lbsyun.baidu.com/index.php?title=yingyan/api/v3/toll)
